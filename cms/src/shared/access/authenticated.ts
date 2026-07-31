811d3524677bff57d5e2ad787d8f370efc7f9648
abhilashlilhore <abhilashlilhore1729@gmail.com>
2026-07-31 12:33:22 +0530

    added console.log

--- cms/src/shared/access/authenticated.ts
+++ cms/src/shared/access/authenticated.ts
@@ -4,5 +4,6 @@ import type { User } from '../../payload-types'
 type isAuthenticated = ({ req: { user } }: AccessArgs<User>) => boolean
 
 export const authenticated: isAuthenticated = ({ req: { user } }) => {
+   console.log("authenticated() user =", user)
   return Boolean(user)
 }
